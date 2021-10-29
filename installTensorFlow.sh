ac13923bede8847bb12b52abe8ba661991132b34
Nathan D <dorman.nathan@gmail.com>
2021-10-29 12:36:19 -0500

    stubbing tensor flow build from source.

---
+++ installTensorFlow.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo 'Installing TensorFlow'
+TF_V='v1.13.1'
+BZ_V='0.19.2'
+# pip install -U --user pip numpy wheel
+# pip install -U --user six mock
+# pip install -U --user keras_preprocessing --no-deps
+# pip install pip --upgrade
+# pip install wheel 
+# pip install six numpy wheel mock
+# pip install keras_applications
+# pip install keras_preprocessing
+
+# git clone https://github.com/tensorflow/tensorflow.git
+# cd tensorflow
+
+# git checkout v1.13.1
+
+# #Bazel block
+# ./configure
+
+
+# #Bazel end
+
+# pip install /tmp/tensorflow_pkg/tensorflow-version-tags.whl
+
+
+
+echo 'Installing TensorFlow complete'
